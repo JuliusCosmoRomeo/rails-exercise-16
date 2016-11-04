cc7ffeb7f4ceb07de081ca73030f1b7165cb2827
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2016-11-04 18:22:15 +0100

    Implemented #2. Closes #2

--- spec/views/authors_view_spec.rb
+++ spec/views/authors_view_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe '/authors/new', type: :view do
+RSpec.describe '/authors/new', :type => :feature do
 	describe "Has first name field"
 		it "checks if the first name field exists" do
 			visit '/authors/new'
