7124cf630f62c4d720376fc3773422f523a67828
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2016-11-06 12:17:35 +0100

    rewrote the test for #12

--- spec/views/authors/show_spec.rb
+++ spec/views/authors/show_spec.rb
@@ -7,14 +7,17 @@ RSpec.describe '/authors/new', type: :view  do
 	describe "Has first name field"
 		it "checks if the first name field exists" do
 			render :template => '/authors/show.html.erb'
-			expect(rendered).to have_text('first name: alan')
+			#expect(rendered).to have_text('first name: alan')
+			expect(rendered).to match /Alan/
 		end
 		it "checks if the last name field exists" do
 			render :template => '/authors/show.html.erb'
-			expect(rendered).to have_text('last name: turing')
+			#expect(rendered).to have_text('last name: turing')
+			expect(rendered).to match /Turing/
 		end
 		it "checks if the homepage field exists" do
 			render :template => '/authors/show.html.erb'
-			expect(rendered).to have_text('homepage: http://wikipedia.de/alan_turing')
+			#expect(rendered).to have_text('homepage: http://wikipedia.de/alan_turing')
+			expect(rendered).to match /http:\/\/wikipedia.de\/Alan_Turing/
 		end
 end
