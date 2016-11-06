9f8d6aac0719c0e0d0f62c9c2c119093243f863f
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2016-11-06 13:26:27 +0100

    edited index spec test for #20

--- spec/views/authors/index_spec.rb
+++ spec/views/authors/index_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe '/authors/index', type: :view  do
 	describe "link to 'show author'-page"
 		it "checks if a css tag 'a' with text 'add author' exists" do
 			render :template => '/authors/index.html.erb'
-			expect(rendered).to have_css("a", text: "Show")
+			expect(rendered).to have_selector "a", text: "Show"
 
 		end
 		
