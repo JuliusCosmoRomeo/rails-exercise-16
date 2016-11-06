9048a9e49dbf081128c2512b7604c03014a668e3
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2016-11-06 12:36:16 +0100

    Test and implementation for #14

---
+++ spec/views/authors/index_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe '/authors/index', type: :view  do
+	before(:context) do
+		@author = create(:author)
+		@authors = Author.all
+	end
+	describe "Contains 'alan turing'"
+		it "checks if 'alan turing' exists" do
+			render :template => '/authors/index.html.erb'
+			expect(rendered).to have_text("Alan Turing")
+		end
+		
+end
