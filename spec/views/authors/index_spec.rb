6c01950c8d10f9c468cae6421f856fe4ef3b15ca
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2016-11-06 13:41:31 +0100

    edited index spec test for #21

--- spec/views/authors/index_spec.rb
+++ spec/views/authors/index_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-require 'capybara/rspec'
+#require 'capybara/rspec'
 RSpec.describe '/authors/index', type: :view  do
 	before(:context) do
 		@author = create(:author)
@@ -13,13 +13,13 @@ RSpec.describe '/authors/index', type: :view  do
 	describe "first and last name in one column"
 		it "checks if a css tag 'th' with text 'name' exists" do
 			render :template => '/authors/index.html.erb'
-			expect(rendered).to have_css("th", text: "Name")
+			expect(rendered).to have_selector "th", text: "Name"
 
 		end
 	describe "link to 'new author'-page"
 		it "checks if a css tag 'a' with text 'add author' exists" do
 			render :template => '/authors/index.html.erb'
-			expect(rendered).to have_css("a", text: "Add author")
+			expect(rendered).to have_selector "a", text: "Add author"
 
 		end
 	describe "link to 'show author'-page"
