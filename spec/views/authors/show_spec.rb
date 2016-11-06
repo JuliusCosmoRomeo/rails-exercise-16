f51c6db0e3dc8c39690265f0837d63423c022155
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2016-11-06 11:46:14 +0100

    additional tests

--- spec/views/authors/show_spec.rb
+++ spec/views/authors/show_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe '/authors/new', type: :view  do
+	before(:context) do
+		@author = create(:author)
+	end
 	describe "Has first name field"
 		it "checks if the first name field exists" do
-			@author = create(:author)
 			render :template => '/authors/show.html.erb'
 			expect(rendered).to have_text(@author.first_name)
 		end
-	
+		it "checks if the last name field exists" do
+			render :template => '/authors/show.html.erb'
+			expect(rendered).to have_text(@author.last_name)
+		end
+		it "checks if the homepage field exists" do
+			render :template => '/authors/show.html.erb'
+			expect(rendered).to have_text(@author.homepage)
+		end
 end
