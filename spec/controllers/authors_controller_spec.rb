145667634fbf69b7c5a23340e3a1f574f58db17e
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2016-11-08 17:35:35 +0000

    hopefully finally resolved ambiguity problem closes #24

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -8,13 +8,11 @@ RSpec.describe AuthorsController, type: :controller do
 		end
 	describe "Show author page"
 		it "renders the authors page" do
-			create(:author)
 			get :show, id: 1
 			expect(response).to render_template("show")
 		end
 	describe "Get authors index page"
 		it "renders the authors index page" do
-			create(:author)
 			get :index
 			expect(response).to render_template("index")
 		end
