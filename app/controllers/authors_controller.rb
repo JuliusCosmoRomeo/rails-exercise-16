e9c9ad53dc4f52c062bbfeee5926d41201833743
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2016-11-09 17:34:41 +0100

    #29 added update method

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -12,6 +12,14 @@ class AuthorsController < ApplicationController
   def edit 
     @author = Author.find(params[:id])
   end
+  def update 
+    @author = Author.find(params[:id])
+    if @author.update(author_params)
+      redirect_to @author
+    else 
+      render 'edit'
+    end
+  end 
   def create 
     @author = Author.new(author_params)
 
