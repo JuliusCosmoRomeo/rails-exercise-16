63045663f098cd08ef0f073ff1c9fa465aff5e41
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2016-11-09 17:48:57 +0100

    added one 'describe' in specs

--- spec/views/authors/edit_spec.rb
+++ spec/views/authors/edit_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe '/authors/edit', :type => :view do
 	before(:each) do
 		@author = create(:author)
 	end
-
-	it "should have author information fields " do
-		render :template => '/authors/edit.html.erb'
-		expect(rendered).to have_field("author_first_name")
-		expect(rendered).to have_field("author_last_name")
-		expect(rendered).to have_field("author_homepage")
-		
+	describe "Author edit"
+		it "should have author information fields " do
+			render :template => '/authors/edit.html.erb'
+			expect(rendered).to have_field("author_first_name")
+			expect(rendered).to have_field("author_last_name")
+			expect(rendered).to have_field("author_homepage")
+			
 	end
 	
 	
