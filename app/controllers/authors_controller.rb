187b23ee64b7fa4719f77a20aab546b4427120bb
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2016-11-09 16:22:30 +0000

    Failing test for #29

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,17 +9,17 @@ class AuthorsController < ApplicationController
     @author = Author.new
 
   end
+  def edit 
+    @author = Author.find(params[:id])
+  end
   def create 
-  	@author = Author.new(author_params)
+    @author = Author.new(author_params)
 
     if @author.save
-    	redirect_to @author
-  	else 
+      redirect_to @author
+    else 
       render "new"
     end  
-  end
-  def edit 
-
   end
   def author_params
   	params.require(:author).permit(:first_name, :last_name, :homepage)
