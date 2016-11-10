870bb053baa410290c08c96c50831881bc895830
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2016-11-10 10:52:18 +0000

    Added 'new paper' page and implementation to make test work

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,2 +1,4 @@
 class PapersController < ApplicationController
+def new
+end 
 end
--- spec/views/papers/new_spec.rb
+++ spec/views/papers/new_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe '/papers/new', :type => :feature do
+RSpec.describe '/papers/new', :type => :view do
 	it "should have title, venue and year field" do
-		visit '/papers/new'
-		expect(page).to have_field("title")
-		expect(page).to have_field("venue")
-		expect(page).to have_field("year")
+		render :template => '/papers/new.html.erb'
+		expect(rendered).to have_field("paper_title")
+		expect(rendered).to have_field("paper_venue")
+		expect(rendered).to have_field("paper_year")
 	end
 	
 end
