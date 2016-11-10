35b7260c6b75c665f8beb25f351b24cc300afb90
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2016-11-10 11:26:40 +0000

    test and implementation for #36

---
+++ spec/models/paper_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Paper, type: :model do
+  #pending "add some examples to (or delete) #{__FILE__}"
+  context "COMPUTING MACHINERY should be a valid paper"
+	  it "should have valid title, venue and year" do
+		  @paper = create(:paper)
+		  expect(@paper.title).to eq('COMPUTING MACHINERY AND INTELLIGENCE')
+		  expect(@paper.venue).to eq('Mind 49: 433-460')
+		  expect(@paper.year).to eq(1950)
+	  end
+end
