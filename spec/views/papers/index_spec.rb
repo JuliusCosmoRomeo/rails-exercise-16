880bb998508c7e8cf690cc3da483124f0338180f
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2016-11-10 18:17:20 +0000

    Failing test for #43 index page contents

---
+++ spec/views/papers/index_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe '/papers/index', :type => :view do
+	it "should have title, venue and year of 'COMPUTING MACHINERY AND INTELLIGENCE'" do
+		@paper = create(:paper)
+		render :template => '/papers/index.html.erb'
+		expect(rendered).to match /COMPUTING MACHINERY AND INTELLIGENCE/
+		expect(rendered).to match /Mind 49: 433-460/
+		expect(rendered).to match /1950/
+	end
+	
+end
