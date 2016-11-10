f6d1a700d9d75fea869bf47e8b2e141eb1d79cee
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2016-11-10 18:40:37 +0000

    Test and impl for #46

--- spec/views/papers/index_spec.rb
+++ spec/views/papers/index_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe '/papers/index', :type => :view do
 		expect(rendered).to match /Mind 49: 433-460/
 		expect(rendered).to match /1950/
 		expect(rendered).to have_selector("a",:text => "Add paper")
+		expect(rendered).to have_selector "a", text: "Show"
+		#expect(rendered).to have_selector "a", text: "Edit"
+		#expect(rendered).to have_selector "a", text: "Destroy"
+		expect(rendered).to have_selector "th", text: "Title"
+		expect(rendered).to have_selector "th", text: "Venue"
+		expect(rendered).to have_selector "th", text: "Year"
+		
 	end
 	
 end
