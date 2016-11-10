cb5a4a1f71be65976b728ee80291f37e41547aae
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2016-11-10 12:08:12 +0000

    Implemented save action for paper

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,4 +1,16 @@
 class PapersController < ApplicationController
 def new
 end 
+def create 
+	@paper = Paper.new(paper_params)
+	@paper.save
+	#if @paper.save
+	#	redirect_to @author
+	#else 
+	render 'new'
+	#end
+end
+def paper_params
+	params.require(:paper).permit(:title, :venue, :year)
+end
 end
