8b9282f113d11c8346a2146ec4644b9345f64f65
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2016-11-12 14:21:58 +0000

    Papers page now includes authors names

--- spec/views/papers/show_spec.rb
+++ spec/views/papers/show_spec.rb
@@ -8,5 +8,13 @@ RSpec.describe '/papers/show', :type => :view do
 		expect(rendered).to match /Venue: Mind 49: 433-460/
 		expect(rendered).to match /Year: 1950/
 	end
+	it "should show the author's name" do
+		@author = create(:author)
+		@paper = create(:paper)
+		@paper.authors.push(@author)
+		render :template => '/papers/show.html.erb'
+		expect(rendered).to match /Alan Turing/
+	end
+	
 	
 end
