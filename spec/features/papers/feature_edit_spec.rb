404c256a32070c6576d995616823ea2505de4eb5
JuliusCosmoRomeo <julius.rudolph@student.hpi.de>
2016-11-12 15:13:38 +0000

    Failing test for #55

--- spec/features/papers/feature_edit_spec.rb
+++ spec/features/papers/feature_edit_spec.rb
@@ -9,4 +9,14 @@ describe "Paper edit page", :type => :feature do
   click_button 'Edit paper'
   expect(Paper.find(@paper.id).year).to eq(1951) 
   end
+  it "should save Peter Plagiarist as author" do
+  @paper = create(:paper)
+  @author2 = create(:author2)
+  visit edit_paper_path(@paper)
+  select "Peter Plagiarist", :from => "paper[author_ids][]"
+  
+  click_button 'Edit paper'
+  expect(Paper.find(@paper.id).year).to eq(1951) 
+  end
+  
 end
